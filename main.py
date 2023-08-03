04470c3c43b4c2c97b4480d94e57d0a954cc94c8
olafrv <olafrv@gmail.com>
2023-08-03 04:39:31 +0200

    split class

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from transformers import TextIteratorStreamer
 # from auto_gptq import AutoGPTQForCausalLM
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama  # type: ignore
+from llama_formatter import llama_formatter
 
 MODELS_METADATA = (
     {
@@ -27,60 +28,6 @@ MODELS_METADATA = (
         "path": "./models"
     }
 )
-
-
-class llama_formatter:
-    # https://github.com/facebookresearch/llama/tree/main
-    # https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L212
-    # https://huggingface.co/TheBloke/Llama-2-13B-chat-GPTQ/discussions/5
-    # https://github.com/facebookresearch/llama/blob/main/llama/tokenizer.py
-
-    BOS, EOS = "<s>", "</s>"  # Sequence tokens
-    B_INST, E_INST = "[INST]", "[/INST]"  # User's instruction tags
-    B_SYS, E_SYS = "<<SYS>>", "<</SYS>>"  # System's response tags
-    DEFAULT_SYSTEM_PROMPT = """This is my default system prompt."""
-    prompts = None
-    prompts_output = ""
-    prompts_index = 0
-
-    def __init__(self):
-        self.prompts = [
-            {"author": "sysdef", "text": self.DEFAULT_SYSTEM_PROMPT}
-        ]
-
-    def add(self, author, text):
-        assert author in ("user", "sys")
-        prompt = {"author": author, "text": text}
-        self.prompts.append(prompt)
-        return format(prompt)
-
-    def concat_last(self, text):
-        self.prompts[-1]["text"] += text
-
-    def update_last(self, text):
-        self.prompts[-1]["text"] = text
-
-    def format(self):
-
-        while self.prompts_index < len(self.prompts):
-
-            author = self.prompts[self.prompts_index]["author"]
-            text = self.prompts[self.prompts_index]["text"]
-
-            if author == "sysdef":
-                # Tokenizer adds BOS at encoding, so BOS not added here
-                self.prompts_output += \
-                    f"{self.B_SYS}{text}{self.E_SYS}{self.EOS}"
-            elif author == "user":
-                if (text and text.strip() != ""):
-                    self.prompts_output += \
-                        f"{self.BOS}{self.B_INST} {text.strip()} {self.E_INST}"
-            elif author == "sys":
-                self.prompts_output += f" {text.strip()}{self.EOS}"
-
-            self.prompts_index += 1
-
-        return self.prompts_output
 
 
 def init_model_and_tokenizer(model_metadata):
